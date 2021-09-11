58a6172dbbf866040940c726477c2c51df14850c
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2021-09-11 13:49:18 -0400

    Add 2021

--- application.py
+++ application.py
@@ -138,7 +138,7 @@ def index():
     inequalities = ["=", ">", "<"]
 
     seasons = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011,
-                2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+                2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
 
     return render_template("index.html", teams=teams, groupings=groupings, filters=filters,
                             inequalities=inequalities, seasons=seasons, play_types=play_types,
