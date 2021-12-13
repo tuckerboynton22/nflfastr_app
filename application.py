36192e19b35b05043751c5cafb1f659ba3ddf6e0
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2021-12-13 13:24:37 -0500

    attempt no. 2 at fixing group ids

--- application.py
+++ application.py
@@ -481,9 +481,9 @@ def results():
 
 
     else:
-        plays = db.execute("SELECT " + grouping + ", COUNT(*) AS total, \
-                            AVG(epa) AS epa, \
-                            AVG(success) AS success, " \
+        plays = db.execute("SELECT " + grouping_id + ", COUNT(*) AS total, \
+                            AVG(epa) AS epa, FIRST(" + group + ") AS " \
+                            + group + " AVG(success) AS success, " \
                             + total + "(" + sort[0] + ") AS total_" + sort[0]  \
                             + ", STRING_AGG(DISTINCT posteam, ', ') as posteam"
                             + " FROM nflfastR_pbp WHERE season>=? AND season<=?" \
