56f38d111aac182ffdf935f64f35a859e3f34da7
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2021-12-13 19:49:39 -0500

    added week description

--- application.py
+++ application.py
@@ -272,16 +272,13 @@ def results():
     if reg_week_query != "" and post_week_query != "":
         week_query = " AND(" + reg_week_query + " OR " + post_week_query + ") "
         week_results = "REG Wks: " + start_reg_week + "-" + end_reg_week + ", POST Wks: " \
-                         + post_weeks.keys()[list(post_weeks.values()).index(start_post_week)] + "-" \
-                              + post_weeks.keys()[list(post_weeks.values()).index(end_post_week)]
+                         + str(start_post_week-17) + "-" + str(end_post_week-17)
     elif reg_week_query != "":
         week_query = " AND" + reg_week_query
         week_results = "REG Wks: " + start_reg_week + "-" + end_reg_week + ", POST Wks: None. "
     elif post_week_query != "":
         week_query = " AND" + post_week_query
-        week_results = "REG Wks: None, POST Wks: " \
-                         + post_weeks.keys()[list(post_weeks.values()).index(start_post_week)] + "-" \
-                              + post_weeks.keys()[list(post_weeks.values()).index(end_post_week)] + ". "
+        week_results = "REG Wks: None, POST Wks: " + str(start_post_week-17) + "-" + str(end_post_week-17)
     else:
         week_query = ""
         week_results = "REG Wks: None, POST Wks: None. "
