1acfdb58e271d0c5d56d34f5d3c2b3e56eccb1d5
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2022-01-17 12:47:40 -0500

    add script to auto-update database

---
+++ nflfastr_database.R
@@ -0,0 +1,14 @@
+library(DBI)
+library(RPostgres)
+library(nflfastR)
+
+future::plan("multisession")
+
+conn <- DBI::dbConnect(RPostgres::Postgres(),
+                       dbname = Sys.getenv("DB_NAME"),
+                       host = Sys.getenv("DB_HOST"),
+                       port = Sys.getenv("DB_PORT"),
+                       user = Sys.getenv("DB_USER"),
+                       password = Sys.getenv("DB_PASSWORD"))
+
+update_db(db_connection = conn, tblname = "nflfastr_pbp", force_rebuild = FALSE)
