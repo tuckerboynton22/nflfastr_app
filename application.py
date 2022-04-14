d4bdb5f973138c7b973ffe289f411b2e6d77562b
Tucker Boynton <70855507+tuckerboynton22@users.noreply.github.com>
2022-04-14 13:54:07 -0400

    Update application.py

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
+
 # Create global filters
 filters = {
     "epa":"Expected Points Added (EPA)",
