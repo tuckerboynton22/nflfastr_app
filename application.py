948cb0c57aa18ebe20d14c459c7447ee677c72d1
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2022-07-27 19:59:14 -0400

    add player specification

--- application.py
+++ application.py
@@ -115,11 +115,6 @@ db = SQL(os.getenv("DATABASE_URL"))
 # db = SQL("sqlite:///cleaned_pbp.db")
 # db = SQL("sqlite:///pbp.db")
 
-passers = db.execute("SELECT DISTINCT passer_id, STRING_AGG(DISTINCT CAST(passer AS TEXT), ', ') AS passer FROM nflfastR_pbp WHERE passer_id !='' GROUP BY passer_id ORDER BY passer")
-names = db.execute("SELECT DISTINCT id, STRING_AGG(DISTINCT CAST(name AS TEXT), ', ') AS name FROM nflfastR_pbp WHERE id !='' GROUP BY id ORDER BY name")
-rushers = db.execute("SELECT DISTINCT rusher_id, STRING_AGG(DISTINCT CAST(rusher AS TEXT), ', ') AS rusher FROM nflfastR_pbp WHERE rusher_id !='' GROUP BY rusher_id ORDER BY rusher")
-receivers = db.execute("SELECT DISTINCT receiver_id, STRING_AGG(DISTINCT CAST(receiver AS TEXT), ', ') AS receiver FROM nflfastR_pbp WHERE receiver_id !='' GROUP BY receiver_id ORDER BY receiver")
-
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
@@ -160,6 +155,11 @@ def homepage():
 # Play index
 @app.route("/index", methods=["GET"])
 def index():
+
+    passers = db.execute("SELECT DISTINCT passer_id, STRING_AGG(DISTINCT CAST(passer AS TEXT), ', ') AS passer FROM nflfastR_pbp WHERE passer_id !='' GROUP BY passer_id ORDER BY passer")
+    names = db.execute("SELECT DISTINCT id, STRING_AGG(DISTINCT CAST(name AS TEXT), ', ') AS name FROM nflfastR_pbp WHERE id !='' GROUP BY id ORDER BY name")
+    rushers = db.execute("SELECT DISTINCT rusher_id, STRING_AGG(DISTINCT CAST(rusher AS TEXT), ', ') AS rusher FROM nflfastR_pbp WHERE rusher_id !='' GROUP BY rusher_id ORDER BY rusher")
+    receivers = db.execute("SELECT DISTINCT receiver_id, STRING_AGG(DISTINCT CAST(receiver AS TEXT), ', ') AS receiver FROM nflfastR_pbp WHERE receiver_id !='' GROUP BY receiver_id ORDER BY receiver")
 
     # Provide form for query
     # teams = db.execute("SELECT DISTINCT posteam FROM nflfastR_pbp WHERE posteam!='' ORDER BY posteam")
