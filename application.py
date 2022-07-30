8872105a562dbd34e9ec39c45adad940fb58165d
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2022-07-30 11:21:12 -0400

    test submit in html

--- application.py
+++ application.py
@@ -209,7 +209,7 @@ def results():
     if request.args.get("name") != "":
         name = request.args.get("name")
 
-    if passer != "" and type(passer) != None:
+    if passer != "":
         player_query = " AND passer_id = '" + passer + "' "
     if receiver != "":
         player_query = player_query + " AND receiver_id = '" + receiver + "' "
