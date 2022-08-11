b12b03c69c420e6e72a95195f6a879fdfbdd0fe9
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2022-08-11 13:52:04 -0400

    remove old unnecessary files from previous data pipeline

--- participation.R
+++
@@ -1,5 +0,0 @@
-library(nflreadr)
-
-participation <- load_participation(seasons = 2016:2021)
-
-write.csv(participation, "/Users/tuckerboynton/Desktop/R/App/nflfastr_app/participation.csv", row.names = F)
