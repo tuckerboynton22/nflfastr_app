02457c2332aab27ac6be435c9429bff84acf5427
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2022-08-11 13:14:19 -0400

    test

--- participation.R
+++ participation.R
@@ -2,4 +2,4 @@ library(nflreadr)
 
 participation <- load_participation(seasons = 2016:2021)
 
-write
+write.csv(participation, "/Users/tuckerboynton/Desktop/R/App/nflfastr_app/participation.csv", row.names = F)
