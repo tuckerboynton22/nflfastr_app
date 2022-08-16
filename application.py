99bb2653f344520fb0a5b0aabc75a4836a81f5e7
tuckerboynton22 <tuckerboynton@college.harvard.edu>
2022-08-16 17:42:46 -0400

    try adding qb page

--- application.py
+++ application.py
@@ -117,7 +117,6 @@ rushers = db.execute("SELECT rusher_id, rusher, posteam FROM rushers")
 names = db.execute("SELECT id, name, posteam FROM names")
 receivers = db.execute("SELECT receiver_id, receiver, posteam FROM receivers")
 players = db.execute("SELECT gsis_id, player, team FROM players")
-qbs = db.execute("SELECT full_name, season, Plays, EPA, CPOE, DVOA, DYAR, PFF, qbr_total, sack_rate FROM qbs")
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
