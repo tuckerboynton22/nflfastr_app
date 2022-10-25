e1a34bb39dff5ba935d31764d2fea5b7c309426e
Tucker Boynton <70855507+tuckerboynton22@users.noreply.github.com>
2022-10-25 18:41:02 -0500

    Update nflfastr_database.R

--- nflfastr_database.R
+++ nflfastr_database.R
@@ -105,7 +105,7 @@ conn <- DBI::dbConnect(RPostgres::Postgres(),
                        user = Sys.getenv("DB_USER"),
                        password = Sys.getenv("DB_PASSWORD"))
 
-nflfastR::update_db(db_connection = conn, tblname = "nflfastr_pbp", force_rebuild = FALSE)
+nflfastR::update_db(db_connection = conn, tblname = "nflfastr_pbp", force_rebuild = TRUE)
 # DBI::dbWriteTable(conn, "nflfastr_pbp", pbp, overwrite = T)
 
 DBI::dbWriteTable(conn, "participation", participation, overwrite = T)
